7e66151055b6c5e406992d87a5f129f521dfd575
MajangGOD <wjdwls48145@gmail.com>
2020-01-06 13:53:43 +0900

    Update InvenClear.php

--- InvenClear/src/InvenClear/InvenClear.php
+++ InvenClear/src/InvenClear/InvenClear.php
@@ -12,12 +12,6 @@ use pocketmine\utils\Config;
 class InvenClear extends PluginBase implements Listener {
 public function onEnable() {
 $this->getServer()->getPluginManager()->registerEvents ( $this, $this);
-$this->Majang = new Config ( $this->getDataFolder() . "Majang.yml" , Config::YAML,
-["인벤초기화" => [ ]]
-);
-
-$this->majang = $this->Majang->getAll();
-$this->getServer()->getPluginManager()->registerEvents ( $this, $this);
 }
 
 public function onCommand(CommandSender $sender, Command $command, $label, array $args): bool{
@@ -30,7 +24,7 @@ $sender->sendMessage ( $tag. "권한이 없습니다." );
 return true;
 }
 if ( ! isset ($args[0])) {
-$sender->sendMessage ($tag . " /인벤 초기화 -자신이 인벤 초기화" );
+$sender->sendMessage ($tag . " /인벤 초기화 - 자신이 인벤 초기화" );
 return true;
 }
 switch ($args[0]) {
